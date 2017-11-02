0626e14e9b4fc33520303ea57e9383c63cc0eb42
Kyle <kyle.mcfar1291@gmail.com>
2017-11-02 12:47:06 -0700

    initial commit

---
+++ Snake.java
@@ -0,0 +1,189 @@
+public class Snake
+{
+  public static void placeFood(int[] position)
+  {
+    StdDraw.setPenColor(StdDraw.BLACK);
+
+    StdDraw.line(position[0] - 0.5, position[1], position[0] + 0.5, position[1]);
+    StdDraw.line(position[0], position[1] + 0.5, position[0], position[1] - 0.5);
+    StdDraw.line(position[0] - .5, position[1] + 0.5, position[0] + 0.5, position[1] - 0.5);
+    StdDraw.line(position[0] - .5, position[1] - 0.5, position[0] + 0.5, position[1] + 0.5);
+  }
+
+  public static SnakeSegment[] addNewSegment(SnakeSegment[] segments, int newSize, int x, int y)
+  {
+    SnakeSegment[] copyArray = new SnakeSegment[newSize];
+    
+    for(int j = 0; j < newSize - 1; j ++)
+    {
+      copyArray[j] = segments[j];
+    }
+
+    copyArray[newSize - 1] = new SnakeSegment();
+    copyArray[newSize - 1].position[0] = x;
+    copyArray[newSize - 1].position[1] = y;
+
+    return(copyArray);
+  }
+
+  public static boolean checkForCollision(SnakeSegment[] segments)
+  {
+    for(int i = 1; i < segments.length; i ++)
+    {
+      if((segments[0].position[0] == segments[i].position[0]) && (segments[0].position[1] == segments[i].position[1]))
+      {
+        return (true);
+      }
+      if((Math.abs(segments[0].position[0]) == 28) || (Math.abs(segments[0].position[1]) == 28))
+      {
+        return (true);
+      }
+    }
+    return(false);
+  }
+
+  public static void main(String[] args)
+  {
+
+    int[] RIGHT = {1, 0};
+    int[] LEFT = {-1, 0};
+    int[] UP = {0, 1};
+    int[] DOWN = {0, -1};
+
+    int gameSpeed = 200;
+    int foodCount = 0;
+
+    char userCommand = 'c';
+    
+    SnakeSegment[] snakeSegments = new SnakeSegment[4];
+
+    for(int i = 0; i < snakeSegments.length; i ++)
+    {
+      snakeSegments[i] = new SnakeSegment();
+    }
+    
+    snakeSegments[0].isHead = true;
+
+    for(int i = 0; i < snakeSegments.length; i ++)
+    {
+      snakeSegments[i].position[0] = 0;
+      snakeSegments[i].position[1] = 0;
+      snakeSegments[i].directionVector[0] = 0;
+      snakeSegments[i].directionVector[1] = 0;
+    }
+
+    StdDraw.setScale(-30, 30);
+
+    StdDraw.clear(StdDraw.BLACK);
+
+    for(int i = 0; i < snakeSegments.length; i ++)
+    {
+      snakeSegments[i].draw(snakeSegments[i].position, snakeSegments[i].directionVector);
+    }
+
+    StdDraw.filledSquare(0, 0, 13);
+    StdDraw.show();
+
+    System.out.println("Welcome! Use wasd to move the snake up, left, down, and right, respectively.");
+
+    int[] foodPosition = new int[2];
+
+    for(int i = 0; i < 2; i ++)
+    {
+      foodPosition[i] = 27 - (int) Math.round(54 * Math.random());
+    }
+
+    while(true)
+    {
+
+      if(StdDraw.hasNextKeyTyped())
+      {
+        userCommand = StdDraw.nextKeyTyped();
+      }
+
+      if(userCommand == 'w')
+      {
+        snakeSegments[0].changeDirection(UP);
+      }
+      else if(userCommand == 'a')
+      {
+        snakeSegments[0].changeDirection(LEFT);
+      }
+      else if(userCommand == 's')
+      {
+        snakeSegments[0].changeDirection(DOWN);
+      }
+      else if(userCommand == 'd')
+      {
+        snakeSegments[0].changeDirection(RIGHT);
+      }
+
+      for(int i = (snakeSegments.length-1); i > 0; i --)
+      {
+        snakeSegments[i].follow(snakeSegments[i-1]); 
+      }
+
+      snakeSegments[0].move(snakeSegments[0].directionVector);
+
+      StdDraw.clear(StdDraw.BLACK);
+ 
+      StdDraw.setPenColor(StdDraw.WHITE);
+      StdDraw.filledSquare(0, 0, 28);
+
+      for(int i = 0; i < snakeSegments.length; i ++)
+      {
+        snakeSegments[i].draw(snakeSegments[i].position, snakeSegments[i].directionVector);
+      }
+
+      placeFood(foodPosition);
+
+      StdDraw.show(gameSpeed);
+
+      if(userCommand != 'c')
+      {
+        if((snakeSegments[0].position[0] != foodPosition[0]) && (snakeSegments[0].position[1] != foodPosition[1]))
+        {
+          if(checkForCollision(snakeSegments))
+          {
+            System.out.println("The snake crashed! GAME OVER");
+            break;
+          }
+        }
+      }
+
+
+      if((snakeSegments[0].position[0] == foodPosition[0]) && (snakeSegments[0].position[1] == foodPosition[1]))
+      {
+
+        foodCount ++;
+
+        snakeSegments = addNewSegment(snakeSegments, snakeSegments.length + 1, foodPosition[0], foodPosition[1]);
+
+        for(int i = 0; i < 2; i ++)
+        {
+          foodPosition[i] = 27 - (int) Math.round(54 * Math.random());
+        }
+
+        if(gameSpeed > 140)
+        {
+          gameSpeed -= 15;
+        }
+        else if(gameSpeed > 90)
+        {
+          gameSpeed -= 10;
+        }
+        else if(gameSpeed > 50)
+        {
+          gameSpeed -= 5;
+        }
+        else if(gameSpeed > 10)
+        {
+          gameSpeed -= 1;
+        }
+      }
+
+    }
+
+    System.out.println("Score: " + foodCount);  
+  }
+}
---
+++ SnakeSegment.java
@@ -0,0 +1,72 @@
+public class SnakeSegment
+{
+
+  public int[] position = new int[2];
+
+  public int[] directionVector = new int[2];
+
+  public boolean isHead;
+
+  public boolean collision = false;
+
+  public void changeDirection(int[] newDirectionVector)
+  {
+    if(((newDirectionVector[0] + directionVector[0]) != 0) ||
+       ((newDirectionVector[1] + directionVector[1]) != 0))
+
+    {
+      directionVector = newDirectionVector;
+    }
+  }
+
+
+  public void move(int[] direction)
+  {
+    if(isHead)
+    {
+      position[0] += direction[0];
+      position[1] += direction[1];
+
+    }
+  }
+
+  public void follow(SnakeSegment segment)
+  {
+    position[0] = segment.position[0];
+    position[1] = segment.position[1];  
+  }
+
+  public void draw(int[] xyCoords, int[] direction)
+  { 
+    if(isHead)
+    {
+      StdDraw.setPenColor(StdDraw.BLUE);
+      if(direction[0] == 1)
+      {
+        StdDraw.line(xyCoords[0], xyCoords[1], xyCoords[0] + 1, xyCoords[1]);
+      }
+      else if(direction[0] == -1)
+      {
+        StdDraw.line(xyCoords[0], xyCoords[1], xyCoords[0] - 1, xyCoords[1]);
+      }
+      else if(direction[1] == 1)
+      {
+        StdDraw.line(xyCoords[0], xyCoords[1], xyCoords[0], xyCoords[1] + 1);
+      }
+      else if(direction[1] == -1)
+      {
+        StdDraw.line(xyCoords[0], xyCoords[1], xyCoords[0], xyCoords[1] - 1);
+      }
+
+    }
+    else
+    {
+      StdDraw.setPenColor(StdDraw.BLACK);
+    }
+
+    StdDraw.filledCircle(xyCoords[0], xyCoords[1], 0.5);
+
+    
+  }
+
+}
